533c22fae993df72ddf8ea41bcc538aed5d56c64
Dave Cromberge <davecromberge@gmail.com>
2015-07-08 15:49:39 +0100

    Solution part 1

--- basics/src/main/scala/flatten/Part01.scala
+++ basics/src/main/scala/flatten/Part01.scala
@@ -27,6 +27,13 @@ trait Part01 {
   }
 
   // Exercise, rewrite the above as a for-comprehension
+  val result2 = for {
+    username <- getUserName(data)
+    user <- getUser(username)
+    email = getEmail(user)
+    validatedEmail <- validateEmail(email)
+    success <- sentEmail(validatedEmail)
+  } yield (success)
 }
 
 trait User
